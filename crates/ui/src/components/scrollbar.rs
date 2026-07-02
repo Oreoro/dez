ab66b946718630bf93532ea8213e67a88dbc195d
Max Katz <me@maxkatz.me>
2026-07-02 14:50:49 +0400

    Reduce scrollbar padding

--- crates/ui/src/components/scrollbar.rs
+++ crates/ui/src/components/scrollbar.rs
@@ -28,7 +28,7 @@ const SCROLLBAR_HIDE_DURATION: Duration = Duration::from_millis(200);
 const SCROLLBAR_SHOW_DURATION: Duration = Duration::from_millis(50);
 
 pub const EDITOR_SCROLLBAR_WIDTH: Pixels = ScrollbarStyle::Editor.to_pixels();
-const SCROLLBAR_PADDING: Pixels = px(4.);
+const SCROLLBAR_PADDING: Pixels = px(2.);
 const BORDER_WIDTH: Pixels = px(1.);
 
 pub mod scrollbars {
